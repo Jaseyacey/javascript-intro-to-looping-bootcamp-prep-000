00606715739be3ada03b748e31b2be0eb4d2fd02
Jason Beedle <jason_beedle@hotmail.com>
2019-03-30 10:39:06 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -26,3 +26,15 @@ function doWhileLoop(array) {
     console.log("I run once regardless.");
   } while (incrementVariable()< array.length);
 }
+function doWhileLoop(num) {
+    var i = 0;
+
+    function incrementVariable() {
+    i = i + 1;
+    return i;
+    }
+
+    do {console.log('I run once regardless');
+    }
+    while (incrementVariable() < num);
+    }
