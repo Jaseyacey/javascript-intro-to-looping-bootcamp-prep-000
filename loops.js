c6c65ba03d6c5fad68500593e86904d3c0374506
Jason Beedle <jason_beedle@hotmail.com>
2019-03-30 09:10:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -6,3 +6,11 @@ for (let i = 0; i < 24; i++) {
     array.push("I am ${i} strange loops.");}
 }return array;
 }
+function forLoop(array) {
+  (let i = 0; i < 24); i++);
+  if (i === 1) {
+    array.push("I am 1 strange loop");
+  } else {
+    array.push("I am ${i} strange loops.")
+  } return(array);
+}
