6ce9689a4178c31024a02fa9deaa799fc46e7117
Jason Beedle <jason_beedle@hotmail.com>
2019-03-30 10:09:58 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,29 +1,3 @@
-function forLoop(array){
-        for ( i = 0; i < 25; i++){
-            if(i === 1){
-                array.push('I am 1 strange loop.');
-            } else {
-                array.push('I am ' + i + ' strange loops.');
-            }
-        } return array;
+function forLoop(array) {
+  let (var i = 0; i < 24; i++) {
     }
-
-    function whileLoop(num){
-      while(num > 0){
-        console.log(num);
-        num--;
-      }
-      return 'done';
-    }
-
-    function doWhileLoop(array){
-            var i = 0;
-
-            function incrementVariable() {
-                i++;
-                return i;
-            }
-            do{
-              console.log('I run once regardless.');
-            } while (incrementVariable() < array.length);
-          }
