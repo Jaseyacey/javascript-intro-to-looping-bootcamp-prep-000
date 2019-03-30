61eb70c1ac24a4e268de412670834949a06b7d8a
Jason Beedle <jason_beedle@hotmail.com>
2019-03-30 10:26:11 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -7,3 +7,11 @@ function forLoop(array){
             }
         } return array;
     }
+
+  function whileLoop(num) {
+    while(num > 0){
+      console.log(num);
+      num--;
+    }
+    return'done';
+  }
